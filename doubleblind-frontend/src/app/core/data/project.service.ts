c096ab469a5ba1ac305ac177d9b4a3cadc430c5a
revol-xut <revol-xut@protonmail.com>
2023-11-24 18:52:38 +0100

    sending to create project with credentials

--- doubleblind-frontend/src/app/core/data/project.service.ts
+++ doubleblind-frontend/src/app/core/data/project.service.ts
@@ -18,29 +18,6 @@ export class ProjectService {
     return this.http.get<Project[]>(`https://api.${API_URL}/project/`,{
       withCredentials: true
     })
-    /*return of([
-      {
-        id: "UUID-1",
-        repo: "https://github.com/tanneberger/test-1",
-        owner: "UID-1",
-        name: "Troll1",
-        last_update: "2023-08-24T20:21Z "
-      },
-      {
-        id: "UUID-2",
-        repo: "https://github.com/MarcelCoding/Foobaar",
-        owner: "UID-1",
-        name: "SeriousHihi",
-        last_update: "2023-10-24T20:21Z "
-      },
-      {
-        id: "UUID-3",
-        repo: "https://github.com/robertro",
-        owner: "UID-1",
-        name: "FooBar",
-        last_update: "2013-12-24T18:21Z "
-      }
-    ])*/
   }
   public getFeaturedProjects() : Observable<Project[]> {
     //return this.http.get<Project[]>(`https://api.${API_URL}/project`);
@@ -74,6 +51,8 @@ export class ProjectService {
     return this.http.post(`https://api.${API_URL}/project/`, {
       name: name,
       repo: repo
+    }, {
+      withCredentials: true
     }).pipe(map(() => void 0));
   }
 }
