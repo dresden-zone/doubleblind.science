bf959edb7705f10ff055a678b582ab6585ade083
revol-xut <revol-xut@protonmail.com>
2023-11-24 15:59:53 +0100

    load all repos on startup

--- doubleblind-frontend/src/app/core/data/repository.service.ts
+++ doubleblind-frontend/src/app/core/data/repository.service.ts
@@ -3,37 +3,34 @@ import {Observable, of} from "rxjs";
 import {Project} from "./project.domain";
 import {Repository} from "./repository.domain";
 import {API_URL} from "./api.domain";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepositoryService {
-
+  private repositories: Repository[] = [];
   constructor(
-    private readonly http: HttpClient
-  ) { }
+    private readonly http: HttpClient,
+  ) {
+    let current_page = 0;
+    let new_repos : Repository[] = [];
+    do {
+      new_repos = [];
+      let http_params = new HttpParams().set('page', current_page).set('per_page', 100);
+      this.http.get<Repository[]>(`https://api.${API_URL}/repositories/`, {
+        withCredentials: true,
+        params: http_params
+      }).forEach((value) => {
+        new_repos.concat(value);
+      }).finally()
+      this.repositories.concat(new_repos);
+      console.log("received {} with page {}", new_repos.length, current_page);
+      current_page += 1;
+    } while (new_repos.length > 0);
+  }
 
   public getRepositories() : Observable<Repository[]> {
-    return this.http.get<Repository[]>(`https://api.${API_URL}/repositories/`,{
-      withCredentials: true
-    })
-    /*return of([
-      {
-        name: "dd-ix/nix-config",
-        logo: "https://avatars.githubusercontent.com/u/110357347?s=48&v=4",
-        repo_id: 3
-      },
-      {
-          name: "tanneberger/bahn.bingo",
-          logo: "https://avatars.githubusercontent.com/u/32239737?s=48&v=4",
-          repo_id: 4
-      },
-      {
-          name: "tlm-solutions/datacare",
-          logo: "https://avatars.githubusercontent.com/u/104242032?s=48&v=4",
-          repo_id: 5
-      }
-    ]);*/
+    return of(this.repositories);
   }
 }
